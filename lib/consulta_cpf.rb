247e4ad746fccfd37195cc4d2f0e694347839d06
Mr Sam <samboy@Mrs-MacBook-Pro.local>
2016-12-05 06:10:34 -0200

    require file

--- lib/consulta_cpf.rb
+++ lib/consulta_cpf.rb
@@ -1,5 +1,6 @@
 require "consulta_cpf/version"
 
+%w(consulta_cpf version).each {|req| require File.dirname(__FILE__) + "/cpf_consulta/#{req}"}
+
 module ConsultaCpf
-  # Your code goes here...
 end
